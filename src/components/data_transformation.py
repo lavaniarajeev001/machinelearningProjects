6732d174003525b9ea516d362541aed7b3d6f489
lavaniarajeev001 <rajeev.lavania@yahoo.co.in>
2024-11-14 10:42:32 +0530

    Model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -93,6 +93,8 @@ class DataTransformation:
                 obj=preprocessing_obj
             )
 
-            return train_arr, test_arr, self.data_transformation_config.preprocessor_obj_file_path
+            return (train_arr, 
+                    test_arr,
+                    self.data_transformation_config.preprocessor_obj_file_path,)
         except Exception as e:
             raise CustomeException(e, sys)
---
+++ src/components/model_trainer.py
@@ -0,0 +1,78 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+
+from src.exception import CustomeException 
+from src.logger import logging
+from src.utils import save_object, evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trained_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Splitting training and test input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1],
+            )
+
+            models = {
+                "Linear Regression": LinearRegression(),
+                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest Regressor": RandomForestRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+            }
+
+            model_report: dict = evaluate_models(
+                X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models
+            )
+            
+            logging.info("Model report generated")
+
+            best_model_score = max(sorted(model_report.values()))
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomeException("No best model found with a satisfactory score.")
+            
+            logging.info(f"Best model '{best_model_name}' found with satisfactory performance.")
+
+            save_object(
+                file_path=self.model_trained_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
+        except Exception as e:
+            raise CustomeException(e, sys)
